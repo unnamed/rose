c5e897e036e4a6f5359a99101c02bd5f2442274e
yusshu <yushu@unnamed.team>
2021-03-13 12:25:57 -0500

    add custom status

--- mod.ts
+++ mod.ts
@@ -1,12 +1,15 @@
-import { startBot, logger } from "./deps.ts";
+import { startBot, logger, editBotsStatus, ActivityType } from "./deps.ts";
 
 import loadListeners from "./loader/listener.loader.ts";
 
 let eventHandlers = await loadListeners();
-eventHandlers.ready = () => logger.info("Successfully connected");
+eventHandlers.ready = () => {
+  logger.info("Successfully connected");
+  editBotsStatus("online", "@Unnamed Bot", ActivityType.Listening);
+};
 
 startBot({
-  token: Deno.env.get('BOT_TOKEN') as string,
+  token: Deno.env.get("BOT_TOKEN") as string,
   intents: ["GUILDS", "GUILD_MESSAGES"],
   eventHandlers
 });
