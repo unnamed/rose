1b295d891f40e45475a11c3957ce57fb2c35bc27
yusshu <yushu@unnamed.team>
2021-03-16 23:20:53 -0500

    add a simple slash commands api wrapper

--- mod.ts
+++ mod.ts
@@ -1,10 +1,36 @@
-import { startBot, logger, editBotsStatus, ActivityType } from "./deps.ts";
+import { startBot, logger, editBotsStatus, ActivityType, SlashCommandOptionType } from "./deps.ts";
 
 import loadListeners from "./loader/listener.loader.ts";
 import loadCommands from "./loader/command.loader.ts";
 
+import { register } from "./command/slash/slash.command.ts";
+
 let eventHandlers = await loadListeners();
+
 eventHandlers.ready = () => {
+
+  register(
+    {
+      guildID: "683899335405994062",
+      name: "hello",
+      description: "Make the bot say hello!",
+    },
+    async interaction => {
+      interaction.sendResponse({
+        private: true,
+        type: 5,
+        data: {
+          content: "Hello!"
+        }
+      });
+      setTimeout(() => {
+        interaction.editResponse({
+          content: "Hi!"
+        });
+      }, 2000);
+    }
+  )
+
   logger.info("Successfully connected");
   editBotsStatus("online", "@Unnamed Bot", ActivityType.Listening);
 };
