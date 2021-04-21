47bae20859e99129d19157a1443af67a5ecc1967
yusshu <yushu@unnamed.team>
2021-04-20 22:38:51 -0500

    remove slash command

--- mod.ts
+++ mod.ts
@@ -3,29 +3,9 @@ import { startBot, logger, editBotsStatus, ActivityType } from "./deps.ts";
 import loadListeners from "./loader/listener.loader.ts";
 import loadCommands from "./loader/command.loader.ts";
 
-import { register } from "./command/slash/slash.command.ts";
-
 let eventHandlers = await loadListeners();
 
 eventHandlers.ready = () => {
-
-  register(
-    {
-      guildID: "683899335405994062",
-      name: "hello",
-      description: "Make the bot say hello!",
-    },
-    async interaction => {
-      interaction.sendResponse({
-        private: true,
-        type: 5,
-        data: {
-          content: "Hello!"
-        }
-      });
-    }
-  )
-
   logger.info("Successfully connected");
   editBotsStatus("online", "@Unnamed Bot", ActivityType.Listening);
 };
