fde91ca63b8a05774ee2e9bf83bd2a4f00dfb130
yusshu <yushu@unnamed.team>
2021-06-17 03:57:38 -0500

    make commit id shown in monospace font

--- src/webhook/github/github.packet.receiver.ts
+++ src/webhook/github/github.packet.receiver.ts
@@ -29,7 +29,7 @@ handlers.set('push', async (client: Client, event: Push) => {
 			.setURL(event.compare)
 			.setAuthor(event.sender.login, event.sender.avatar_url, event.sender.html_url)
 			.setDescription(
-				event.commits.map(commit => `[${commit.id.substring(0, 6)}](${commit.url}) ${commit.message}`)
+				event.commits.map(commit => `\`[${commit.id.substring(0, 6)}](${commit.url})\` ${commit.message}`)
 			)
 	);
 });
