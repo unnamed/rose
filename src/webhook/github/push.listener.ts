4358dec71fbe1ba6da30997cb4e1043d0184822e
yusshu <yushu@unnamed.team>
2021-06-17 10:51:20 -0500

    merge commit history in embeds

--- src/webhook/github/push.listener.ts
+++ src/webhook/github/push.listener.ts
@@ -2,17 +2,45 @@ import {Push} from './github.events';
 import {Client, MessageEmbed, TextChannel} from 'discord.js';
 import config from '../../config';
 
+let lastEmbedMessageId: string | undefined = undefined;
+
 export default async (client: Client, event: Push) => {
 	const channel = await client.channels.fetch('805139625256419338') as TextChannel;
 	const branch = event.ref.split('/').pop();
 	const repo = event.repository.full_name;
-	await channel.send(
-		new MessageEmbed()
-			.setColor(config.color)
-			.setAuthor(
-				`${repo}:${branch}`,
-				channel.guild.iconURL({ size: 64, format: 'png' })
-			)
-			.setDescription(event.commits.map(commit => `[\`${commit.id.substring(0, 6)}\`](${commit.url})  ${commit.message}`).join('\n'))
-	);
-}
+
+	const history = event.commits
+		.map(commit => `[\`${commit.id.substring(0, 6)}\`](${commit.url})  ${commit.message}`)
+		.join('\n');
+
+	const title = `${repo}:${branch}`;
+	const iconURL = channel.guild.iconURL({size: 64, format: 'png'});
+
+	async function sendNewEmbed() {
+		lastEmbedMessageId = (await channel.send(
+			new MessageEmbed()
+				.setColor(config.color)
+				.setAuthor(title, iconURL)
+				.setDescription(history)
+		)).id;
+	}
+
+	if (!lastEmbedMessageId) {
+		await sendNewEmbed();
+	} else {
+		const lastEmbedMessage = await channel.messages.fetch(lastEmbedMessageId);
+		const lastEmbed = lastEmbedMessage.embeds[0];
+
+		if (lastEmbed.author.name !== title
+			&& (lastEmbed.description.match(/\n/g) ?? []).length <= 8) {
+			await sendNewEmbed();
+		} else {
+			await lastEmbedMessage.edit(
+				new MessageEmbed()
+					.setColor(config.color)
+					.setAuthor(title, iconURL)
+					.setDescription(lastEmbed.description + '\n' + history)
+			);
+		}
+	}
+};
