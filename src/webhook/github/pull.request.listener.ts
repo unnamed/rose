44ec2f898dc349b97b8617115dd7b79d6e627284
yusshu <yushu@unnamed.team>
2021-06-21 09:54:06 -0500

    use full_name instead of name and add a '#' before the PR number

--- src/webhook/github/pull.request.listener.ts
+++ src/webhook/github/pull.request.listener.ts
@@ -10,11 +10,11 @@ export default async (client: Client, event: PullRequestEvent) => {
 			new MessageEmbed()
 				.setColor(config.color)
 				.setAuthor(
-					`${event.repository.name} | Pull request opened by ${event.pull_request.user.login}`,
+					`${event.repository.full_name} | Pull request opened by ${event.pull_request.user.login}`,
 					channel.guild.iconURL({ size: 64, format: 'png' }),
 					event.pull_request.html_url
 				)
-				.setTitle(`${event.number} ${event.pull_request.title}`)
+				.setTitle(`#${event.number} ${event.pull_request.title}`)
 				.setDescription(event.pull_request.body)
 		);
 	}
