7adf6912d520c7d472b1146efe7cf7995884fb8f
yusshu <yushu@unnamed.team>
2021-07-13 12:08:50 -0500

    fix, use busboy instead of multer

--- src/http/resourcepack/resourcepack.ts
+++ src/http/resourcepack/resourcepack.ts
@@ -1,17 +1,19 @@
+import fs from 'fs';
 import express from 'express';
-import multer from 'multer';
 import {getApp} from '../server';
 import config from '../../config';
 
-const upload = multer({dest: 'resourcepack/'});
+
+const destination = `${__dirname}/data`;
+const fileName = 'resource-pack.zip';
+const filePath = destination + '/' + fileName;
 
 export async function startResourcePackServer() {
 	const app: express.Application = await getApp();
 	app.post(
 		config.http.resourcePack.route,
-		upload.single('resourcepack'),
-		(req, res, next) => {
-			if (req.headers['X-ResourcePack-Token'] !== process.env.RESOURCE_PACK_SECRET) {
+		(req, res) => {
+			if (req.headers['x-unnamed-authorization'] !== process.env.RESOURCE_PACK_SECRET) {
 				res.status(500).json({
 					code: 500,
 					error: 'Invalid authorization!'
@@ -19,13 +21,19 @@ export async function startResourcePackServer() {
 				return;
 			}
 
-			next();
+			req.pipe(req['busboy']);
+			req['busboy'].on('file', (fieldName, file) => {
+				const stream = fs.createWriteStream(filePath);
+				file.pipe(stream);
+			});
+
+			res.status(200).json({ status: 'ok' });
 		});
 
 	app.get(
 		config.http.resourcePack.route,
 		(req, res) => {
-			res.download(`${__dirname}/resourcepack`);
+			res.download(filePath);
 		}
 	);
 }
