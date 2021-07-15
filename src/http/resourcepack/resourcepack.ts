c07b6b102a5a192198ad5f88d3c31d2678b6ec6a
yusshu <yushu@unnamed.team>
2021-07-15 09:28:18 -0500

    hash

--- src/http/resourcepack/resourcepack.ts
+++ src/http/resourcepack/resourcepack.ts
@@ -1,3 +1,4 @@
+import crypto from 'crypto';
 import express from 'express';
 import {getApp} from '../server';
 import config from '../../config';
@@ -21,22 +22,28 @@ export async function startResourcePackServer(client: Client) {
 			}
 
 			const file = req['files']['resourcepack'];
+			console.log(file);
 
 			file.mv(filePath, err => {
 				if (err) {
 					res.status(500).send(err);
 				} else {
+					const hashSum = crypto.createHash('sha256');
+					hashSum.update(file.data);
+					const hash = hashSum.digest('hex');
+
 					client.channels.fetch('820071190457614357')
 						.then(channel => (channel as TextChannel).send(
 							new MessageEmbed()
 								.setColor(config.color)
 								.setTitle('Development Server | Logs')
-								.setDescription('Received resource pack update')
+								.setDescription(`Received resource pack update, hash: \`${hash}\``)
 								.setTimestamp()
 						));
 					res.status(200).json({
 						status: 'ok',
-						url: 'https://artemis.unnamed.team/resource-pack'
+						url: 'https://artemis.unnamed.team/resource-pack',
+						hash
 					});
 				}
 			});
