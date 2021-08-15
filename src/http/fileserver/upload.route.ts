f48922d547caa31ffe48021cd1b070bf13b35224
yusshu <yushu@unnamed.team>
2021-08-15 12:51:04 -0500

    add some checks and comments to the upload route code

--- src/http/fileserver/upload.route.ts
+++ src/http/fileserver/upload.route.ts
@@ -12,17 +12,36 @@ export default (
 ) => {
   router.post('/upload/:id', (req, res) => {
 
+    // application id
     const id = req.params.id;
 
-    if (req.headers['authorization'] !== process.env[`APPLICATION_${id}_RESOURCE_PACK_SECRET`]) {
-      res.status(500).json({
-        code: 500,
-        error: 'Invalid authorization!'
+    // function to set status code and error
+    function fail(code: number, error: string) {
+      res.status(code).json({
+        ok: false,
+        code,
+        error
       });
-      return;
     }
 
-    const file = req['files']['resourcepack'];
+    // check if has a valid authorization token
+    if (req.headers['authorization'] !== process.env[`APPLICATION_${id}_RESOURCE_PACK_SECRET`]) {
+      return fail(401, 'Invalid authorization token');
+    }
+
+    // look for uploaded files
+    const files = req['files'];
+
+    if (!files) {
+      return fail(400, 'No files specified');
+    }
+
+    // TODO: Not only accept 'resourcepack' file
+    const file = files['resourcepack'];
+
+    if (!file) {
+      return fail(400, 'No "resourcepack" file specified');
+    }
 
     file.mv(path.join(dataDir, id), err => {
       if (err) {
@@ -30,11 +49,13 @@ export default (
         return;
       }
 
+      // compute file hash
       const hashSum = crypto.createHash('sha1');
       hashSum.update(file.data);
       const hash = hashSum.digest('hex');
       const downloadUrl = `https://artemis.unnamed.team${moduleConfig.route}/get/${id}`;
 
+      // notify about the upload
       client.channels.fetch('820071190457614357')
         .then(channel => (channel as TextChannel).send(
           new MessageEmbed()
@@ -46,11 +67,14 @@ export default (
             )
             .setDescription('Received resource pack update')
             .addField('Hash (sha1)', '```' + hash + '```', true)
+            .addField('Size', '```' + file.size + '```', true)
             .addField('Application', '```' + id + '```', true)
             .addField('User Agent', '```' + req.headers['user-agent'] + '```', true)
             .addField('Download URL', '```' + downloadUrl + '```', true)
             .setTimestamp()
         ));
+
+      // finally set the response
       res.status(200).json({
         status: 'ok',
         url: downloadUrl,
