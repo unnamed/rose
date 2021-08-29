6d2c5279bc0f9817f2d3e915c4a74fe6a553d471
yusshu <yushu@unnamed.team>
2021-08-28 21:30:32 -0500

    fix(temporal): don't send embeds if 'title' or 'summary' aren't valid

--- src/http/github/check.run.listener.ts
+++ src/http/github/check.run.listener.ts
@@ -3,6 +3,15 @@ import {CheckRun} from './github.events';
 import config from '../../config';
 
 export default async (client: Client, event: CheckRun) => {
+
+  const { title, summary } = event.check_run.output;
+
+  if (!title || !summary) {
+    // temporal bugfix, don't send anything if title or
+    // summary are null
+    return;
+  }
+
   // TODO: de-hardcode channels!
   const channel = await client.channels.fetch('805139625256419338') as TextChannel;
   const iconURL = channel.guild.iconURL({size: 64, format: 'png'});
@@ -11,10 +20,10 @@ export default async (client: Client, event: CheckRun) => {
     new MessageEmbed()
       .setColor(config.color)
       .setAuthor(
-        `${event.repository.full_name} | ${event.check_run.output.title}`,
+        `${event.repository.full_name} | ${title}`,
         iconURL,
         event.repository.html_url
       )
-      .setDescription(event.check_run.output.summary)
+      .setDescription(summary)
   );
 };
