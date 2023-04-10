15e88c00ebd131faccd7bb8126ea7d81c1fd6391
yusshu <yushuverduras1309@gmail.com>
2023-04-10 02:38:19 -0500

    feat: add command cooldown (5 seconds)

--- src/bot/command.loader.ts
+++ src/bot/command.loader.ts
@@ -1,6 +1,6 @@
 import signale from 'signale';
 import { Command } from './command/command.builder';
-import { Client, CommandInteraction, MessageEmbed } from 'discord.js';
+import {Client, CommandInteraction, MessageEmbed, Snowflake} from 'discord.js';
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
 import config from '../../config';
@@ -8,6 +8,8 @@ import config from '../../config';
 import AnnounceCommand from './command/announce.command';
 import PingCommand from './command/ping.command';
 import RepoCommand from './command/repo.command';
+
+const COOLDOWN_EXPIRY_TIME = 5 * 1000;
 
 export default async function loadCommands(client: Client) {
 
@@ -28,6 +30,18 @@ export default async function loadCommands(client: Client) {
 
   signale.success('Successfully loaded %s commands', commands.length);
 
+  const commandCoolDownExpiryTimestamps = new Map<Snowflake, number>();
+
+  // task to clean up the 'commandCoolDownExpiryTimestamps' map
+  setInterval(() => {
+    const now = Date.now();
+    for (const [ id, expiry ] of commandCoolDownExpiryTimestamps) {
+      if (expiry <= now) {
+        commandCoolDownExpiryTimestamps.delete(id);
+      }
+    }
+  }, 5000);
+
   // start listening for command interactions
   client.on('interactionCreate', async interaction => {
 
@@ -41,6 +55,27 @@ export default async function loadCommands(client: Client) {
     if (!command) {
       return;
     }
+
+    const member = commandInteraction.member;
+    const expiry = commandCoolDownExpiryTimestamps.get(member.user.id);
+    const now = Date.now();
+
+    // did it not expire yet?
+    if (expiry !== undefined && expiry > now) {
+      await commandInteraction.reply({
+        ephemeral: true,
+        embeds: [
+          {
+            title: 'Hey, you\'re going to burn your keyboard!',
+            description: `You have to wait ${Math.round(COOLDOWN_EXPIRY_TIME / 1000)} seconds to do this again`,
+            color: config.color,
+          } as MessageEmbed
+        ]
+      });
+      return;
+    }
+
+    commandCoolDownExpiryTimestamps.set(member.user.id, now + COOLDOWN_EXPIRY_TIME);
 
     try {
       await command.executor(commandInteraction);
